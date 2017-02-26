d492595cd6f14666ca7774c7c505a862ab820e48
ZongAng123 <zongang15@yahoo.com>
2017-02-26 19:08:16 +0800

    Initial Commit

---
+++ TableView界面传值/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView界面传值
+//
+//  Created by 纵昂 on 2017/2/26.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
